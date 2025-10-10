8f1f9f2f71b2bb85b6c1c3e46bac4bbf961799da
Alexander <alexandergts162000@gmail.com>
2025-10-10 13:06:07 -0600

    feat: v2.0 from input hex component

--- src/lib/components/modules/InputHex/InputHex.svelte
+++ src/lib/components/modules/InputHex/InputHex.svelte
@@ -2,21 +2,47 @@
     import chroma from 'chroma-js';
     import type { Writable } from 'svelte/store';
 
-    export let colorStore: Writable<chroma.Color>; 
+    export let colorStore: Writable<chroma.Color>;
 
-    let inputHexValue: string; 
+    // Ajustar estas lineas si es necesario
+    export const borderValid: string = 'border-gray-300';  
+    export const borderInvalid: string = 'border-red-300'; 
 
-    // Actualizar el STORE (Input -> Store)
-    $: if (inputHexValue && chroma.valid(inputHexValue)) {
-        const newColor = chroma(inputHexValue);
-        
-        // 🚨 CAMBIO CLAVE: Usamos la prop 'colorStore' para actualizar el global
-        colorStore.set(newColor); 
+    export let weight: string = 'w-[200px]'
+
+    let inputHexValue: string = '';
+    let isInvalid = false;
+    let isUserEditing = false;
+
+    // Store -> Input
+    $: if ($colorStore && !isUserEditing) {
+        inputHexValue = $colorStore.hex();
     }
 
-    // Actualizar el INPUT (Store -> Input)
-    $: if ($colorStore) {
-        inputHexValue = $colorStore.hex();
+    // Input -> Store
+    function handleInput() {
+        isUserEditing = true;
+        
+        const sanitizedValue = inputHexValue.replace(/^#/, '').trim();
+        
+        // Validar
+        const isValid = sanitizedValue.length > 0 && chroma.valid(`#${sanitizedValue}`);
+        isInvalid = sanitizedValue.length > 0 && !isValid;
+        
+        // Actualizar
+        if (isValid) {
+            colorStore.set(chroma(`#${sanitizedValue}`));
+        }
+    }
+
+    // Detectar edición
+    function handleBlur() {
+        isUserEditing = false;
+        
+        if (isInvalid || inputHexValue.trim() === '') {
+            inputHexValue = $colorStore.hex();
+            isInvalid = false;
+        }
     }
 </script>
 
@@ -24,5 +50,10 @@
     type="text" 
     placeholder="#HEX"
     bind:value={inputHexValue}
-    class="bg-white p-1 rounded-lg"
+    on:input={handleInput}
+    on:blur={handleBlur}
+    class={`
+        ${weight} bg-gray-50 px-2 py-1 rounded-lg border-2 outline-none
+        ${isInvalid ? borderInvalid : borderValid}
+    `}
 />
